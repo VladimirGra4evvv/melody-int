c869807ed2039c81e159464139a04b4b07919b31
VladimirGra4evvv <gra4evvv@yandex.ru>
2021-08-20 09:22:19 +0300

    added: counter in day 2-3

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var currentFloor = 2;
+  var counterUp = $(".counter-up");
+  var counterDown = $(".counter-down");// переменная где хранится текущий этаж
+  var floorPath = $(".home-image path"); // каждый отдельный этаж в svg
+  // console.log("сайт готов к манипуляциям") //проверка работы скрипта
+  
+  //функция при наведении мышкой на этаж
+  floorPath.on("mouseover", function () {
+    currentFloor = $(this).attr("data-floor");//удаляем активный класс у этажей
+    $(".counter").text(currentFloor);// получаем значение текущего этажа
+    floorPath.removeClass("current-floor");// записываем значение этажа в счетчик справа
+  });
+
+  counterUp.on("click", function() { // отслеживание клика по кнопке вверх
+    if(currentFloor < 18 ) { // проверяем значение этажа
+      currentFloor++; // прибавляем один этаж
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false }); //форматируем переменную с этажом, чтобы было 01 а не 1
+      $(".counter").text(usCurrentFloor); // записываем значения этажа в счетчик справа
+      floorPath.removeClass("current-floor");//удаляем активный класс у этажей
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");//подсвечиваем текущий этаж
+    }
+  });
+
+  counterDown.on("click", function() {
+    if(currentFloor > 2 ) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+});
