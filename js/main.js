df4f15d6db77b7c2e13c2df8c642ed04a0bc8c7f
VladimirGra4evv <gra4evvv@yandex.ru>
2021-08-20 20:22:33 +0300

    added: day4

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ $(document).ready(function () {
   var floorPath = $(".home-image path"); // каждый отдельный этаж в svg
   var modal = $(".modal");
   var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
   // console.log("сайт готов к манипуляциям") //проверка работы скрипта
-  
+
   //функция при наведении мышкой на этаж
   floorPath.on("mouseover", function () {
     currentFloor = $(this).attr("data-floor");//удаляем активный класс у этажей
@@ -14,24 +15,24 @@ $(document).ready(function () {
     floorPath.removeClass("current-floor");// записываем значение этажа в счетчик справа
   });
 
-floorPath.on('click', toggleModal); //при клике на этаж вызывает окно
+  floorPath.on('click', toggleModal); //при клике на этаж вызывает окно
+  modalCloseButton.on('click', toggleModal);
+  viewFlatsButton.on('click', toggleModal); //клик на кнопку закрыть убирает окно
 
-modalCloseButton.on('click', toggleModal); //клик на кнопку закрыть убирает окно
-
-  counterUp.on("click", function() { // отслеживание клика по кнопке вверх
-    if(currentFloor < 18 ) { // проверяем значение этажа
+  counterUp.on("click", function () { // отслеживание клика по кнопке вверх
+    if (currentFloor < 18) { // проверяем значение этажа
       currentFloor++; // прибавляем один этаж
-      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false }); //форматируем переменную с этажом, чтобы было 01 а не 1
+      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false }); //форматируем переменную с этажом, чтобы было 01 а не 1
       $(".counter").text(usCurrentFloor); // записываем значения этажа в счетчик справа
       floorPath.removeClass("current-floor");//удаляем активный класс у этажей
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");//подсвечиваем текущий этаж
     }
   });
 
-  counterDown.on("click", function() {
-    if(currentFloor > 2 ) {
+  counterDown.on("click", function () {
+    if (currentFloor > 2) {
       currentFloor--;
-      usCurrentFloor = currentFloor.toLocaleString("en-US", {minimumIntegerDigits: 2, useGrouping: false });
+      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
       $(".counter").text(usCurrentFloor);
       floorPath.removeClass("current-floor");
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
